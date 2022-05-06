b5901f420f3a3b4c6fe9eb532f0dac3a89da2db3
dahye <>
2022-05-06 17:23:51 +0900

    🎉initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from pymongo import MongoClient
+from datetime import datetime, timedelta
+import hashlib
+
+app = Flask(__name__)
+
+client = MongoClient('127.0.0.1', 27017, username="test", password="test")
+db = client.developITdb
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
