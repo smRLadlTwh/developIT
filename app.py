33dce99f6952b870ab8ec5cd7efb8982caae7137
dahye <>
2022-05-09 14:15:43 +0900

    ✨ 게시글 업로드 - 게시글 페이지 반환 구현

--- app.py
+++ app.py
@@ -10,3 +10,9 @@ db = client.developITdb
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
+
+# 게시글 페이지 띄우기
+@app.route("/board")
+def board():
+    return render_template('board.html')
+
