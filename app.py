0d601a9a0b71d9c69aea36b48919aae760fc398d
eet43 <1998opening@gmail.com>
2022-05-09 14:40:32 +0900

    test

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ app = Flask(__name__)
 client = MongoClient('127.0.0.1', 27017, username="test", password="test")
 db = client.developITdb
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
