ef1401a7ec2a71e614da86a1c6a9a8c7bd234cbc
dahye <>
2022-05-09 16:30:42 +0900

    no message

--- README.md
+++
@@ -1,2 +0,0 @@
-# developIT-backend
-느낌있조 프로젝트 백엔드
--- exception.py
+++
@@ -1,8 +0,0 @@
-from flask import abort, jsonify, Flask
-
-app = Flask(__name__)
-
-
-@app.errorhandler(404)
-def resource_not_found(e):
-    return jsonify(error=str(e)), 404
