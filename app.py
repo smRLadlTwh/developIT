ffb8f56112b6f41ccc242bb5aa4802247387a147
dahye <>
2022-05-10 12:43:51 +0900

    ✨전체 게시물 내려주기 - 전체 게시물 내려주기 구현

--- app.py
+++ app.py
@@ -58,5 +58,12 @@ def board_write():
     return jsonify(response)
 
 
+# 게시글 전체 보여주기 API
+@app.route("/api/board", methods=['GET'])
+def board_entire_show():
+    response = board.board_show()
+    return jsonify(response)
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
