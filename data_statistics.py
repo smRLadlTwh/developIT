f8756bcd748d68619b7dbd9f922a3ee931ea4a61
dahye <>
2022-05-12 17:59:43 +0900

    ⚡️ 달/년이 바뀌는 경우 예외처리 추가

--- data_statistics.py
+++ data_statistics.py
@@ -1,6 +1,7 @@
 import datetime
 import os
 import re
+import calendar
 
 from pymongo import MongoClient
 
@@ -19,23 +20,58 @@ db = client.developITdb
 
 def statistics():
     now = datetime.datetime.now()
-    time = now.strftime('%Y-%m-%d')
-    today = time + ".*"
-    like_search = re.compile(today, re.IGNORECASE)
+    time = now.strftime('%Y-%m')
+    y = int(now.strftime('%Y'))
+    m = int(now.strftime('%m'))
+    day = int(now.strftime('%d')) - 4
 
-    board_count = db.board.estimated_document_count({})
-    user_count = db.user.estimated_document_count({})
-    today_board_count = len(list(db.board.find({'board.created_at': like_search})))
-    today_user_count = len(list(db.user.find({'user.created_at': like_search})))
+    board = []
+    user = []
+    label = []
+
+    count = 0
+    i = 0
+    while count < 5:
+        search_day = int(day) + i
+        # 달이 바뀌는 경우
+        if search_day == int(calendar.monthrange(y, m)[1]) + 1:
+            search_day = 1
+            day = 1
+            i = 0
+            m = int(now.strftime('%m')) + 1
+            # 년이 바뀌는 경우
+            if m == 13:
+                y = int(now.strftime('%Y')) + 1
+                m = 1
+
+            if m < 10:
+                time = str(y) + '-0' + str(m)
+            else:
+                time = str(y) + '-' + str(m)
+
+        # 1의 자리 숫자인 경우 0을 붙임 ex 8 -> 08
+        if search_day < 10:
+            search_day = '0' + str(search_day)
+        search_time = time + '-' + str(search_day)
+        print(search_time)
+        today = search_time + ".*"
+        like_search = re.compile(today, re.IGNORECASE)
+        board_count = len(list(db.board.find({'board.created_at': like_search})))
+        user_count = len(list(db.user.find({'user.created_at': like_search})))
+
+        board.append(board_count)
+        user.append(user_count)
+        label.append(search_time)
+        i = i + 1
+        count = count + 1
 
     response = {
         'result': 'success',
-        'time': time,
+        'time': time + '-' + str(day),
         'data': {
-            'board_count': board_count,
-            'user_count': user_count,
-            'today_board_count': today_board_count,
-            'today_user_count': today_user_count,
+            'board': board,
+            'user': user,
+            'label': label,
         },
         'status_code': 200
     }
