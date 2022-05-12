ce000838366f01482dc407f507412ffb6b5b93b4
dahye <>
2022-05-12 11:59:37 +0900

    로그인 실패 문구 수정

--- configs/config_local.py
+++ configs/config_local.py
@@ -5,4 +5,4 @@ aws_secret_key = 'xoaoWezduc2bmfzh3+Rj+0B6l/raUZEdIP5ECjPs'
 CLIENT_ID = '8859c12e71f0f0364726824a85c1fb2e'
 REDIRECT_URI = 'http://localhost:5000/oauth'
 CLIENT_SECRET = "eOVDwn9YNmJ2M2x8mm0wbGLklqtMy23Q"
-SIGNOUT_REDIRECT_URI = "/"
+SIGNOUT_REDIRECT_URI = "http://localhost:5000/"
